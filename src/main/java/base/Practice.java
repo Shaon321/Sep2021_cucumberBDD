a1c33555b7b589e41840e2d473b6e5c8b80d2f46
Shaon321 <lumaknun@gmail.com>
2021-10-01 12:56:40 -0700

    First HomeWrok

---
+++ src/main/java/base/Practice.java
@@ -0,0 +1,10 @@
+package base;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+
+public class Practice {
+    public static void main(String[] args){
+        String x ="test";
+        System.out.println(x);
+    }
+}
